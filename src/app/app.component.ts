5460b696320c4d1107e9a78f2c267e43bfc08cee
Juan8516 <juan.ind85@gmail.com>
2023-12-22 21:11:16 -0500

    chore: :sparkles: Se agrega informacion a la pagina principal.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'basesAngular';
+  public title:   string = 'AngularApp';
+  public counter: number = 10;
 }
