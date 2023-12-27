a4d7bad207befc35cfe9dc7d4ada8b29117c1aa6
Juan8516 <juan.ind85@gmail.com>
2023-12-27 18:39:13 -0500

    style(Styles): :lipstick: Se aplica estilos al encabezado, contador y botones.

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -3,21 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-counter',
   template: `
-    <h3 class="font-fontEncabezado">Counter: {{ counter }}</h3>
+  <div class="flex justify-center">
+    <p class="flex justify-center mt-5 mb-5
+        box-border h-45 w-40 pb-5 border-4
+        bg-yellow-200
+        text-lime-900 text-9xl
+        text-center">
+      {{ counter }}</p>
+    </div>
 
     <div class="flex justify-evenly mt-4 mb-4">
       <button
-        class="bg-[#9AD0C2] hover:bg-[#2D9596] text-white font-bold py-6 px-8 rounded"
+        class="font-fontContador text-5xl bg-[#9AD0C2] hover:bg-[#2D9596] text-white font-bold py-6 px-8 rounded"
         (click)="increaseBy(-1)"
       >-</button>
 
       <button
-        class="bg-[#2D9596] hover:bg-[#9AD0C2] text-white font-bold py-6 px-12 rounded"
+        class=" font-fontContador text-5xl bg-[#2D9596] hover:bg-[#9AD0C2] text-white font-bold py-6 px-12 rounded"
         (click)="resetCounter()">
       Reset</button>
 
       <button
-        class="bg-[#9AD0C2] hover:bg-[#2D9596] text-white font-bold py-6 px-8 rounded"
+        class="font-fontContador text-5xl bg-[#9AD0C2] hover:bg-[#2D9596] text-white font-bold py-6 px-8 rounded"
         (click)="increaseBy(+1)">
       +</button>
     </div>
